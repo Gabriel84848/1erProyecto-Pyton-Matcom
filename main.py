13013c1c4a921b3056f114d488273b35acb35f88
Gabriel84848 <Gabrieladrian84848@gmail.com>
2025-12-29 16:56:44 -0500

    menu 1

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from guardar_y_cargar import cargar_Habitaciones, guardar_reservas
+import os
 
-#def console.clear pero en pyton
+def clear():
+    os.system("cls" if os.name == "nt" else "clear")
 
 def Menu():
 
@@ -12,31 +14,52 @@ def Menu():
     print("4. Crear nueva reserva ")
     print("5. Cancelar reserva existente")
     print("6. Guardar cambios")
+    print("7. Salir")
 
-    return input("selecciona una opcion (1 - 6): ")
+    return input("selecciona una opcion (1 - 7): ")
+
+def ver_habitaciones(habitaciones):
+
+    print("Habitaciones disponibles")
+
+    print("PISO 1:")
+    count=0
+    for hab in habitaciones:
+        if hab.piso == 1:
+            count +=1
+            print(f"{count}: {hab}")
+    
+    print("PISO 2:")
+    count=0
+    for hab in habitaciones:
+        if hab.piso == 2:
+            count +=1
+            print(f"{count}: {hab}")
     
 def main():
 
     print("BLUE GATE HOTEL - Reservas")
-
     habitaciones, servicios, reservas = cargar_Habitaciones()
 
-    print(f"\n ESTADISTICAS DEL HOTEL")
-    print(f"habitaciones: {len(habitaciones)}")
-    print(f"servicios: {len(servicios)}")
-    print(f"reservas: {len(reservas)}")
+    while True:
+        clear()
+        opcion = Menu()
 
-    if habitaciones:
+        if opcion == "1":
+            clear()
+            ver_habitaciones(habitaciones)
+            input("\nPresiona Enter para volver al menu")
         
-        print(f"primera habitacion")
-        print(f"{habitaciones[0]}")
+        elif opcion == "7":
+            clear()
+            print("Saliste")
+            break
+        
+        else:
+            print("NO")
+            input("\nPresiona Enter para volver al menu")
 
-        print(f"servicios disponibles")
-        for servicio in servicios:
-            print(f"primera habitacion")
-        print(f"{servicio}")
-
-if "a":
+if True:
     main()
 
 
